a1f4112f087413fdd3b426a7a93d6c78d59896d5
Luciana Artero <luart16@gmail.com>
2024-05-28 20:15:51 -0300

    creación de repositorio

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import formulario from "./components/Formulario.vue" /*acá importo desde el componente formulario*/ 
+</script>
+
+<template>
+  
+  <formulario/> <!--acá llamo lo que importé arriba en script-->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Mensaje de prueba de importación</h1>
+</template>
+
+<style>
+h1{
+    font-size: 100px;
+    color: red;
+}
+
+</style>
