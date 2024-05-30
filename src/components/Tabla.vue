7d421709ccc2d8483fff5cd1ea346006efd9f429
Paula Mari <paulaelenamari@gmail.com>
2024-05-30 11:26:26 -0300

    Tabla arreglada

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -1,49 +1,49 @@
 <template>
-    <div class="Tabla-Juegos">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Plataforma</th>
-                    <th>Estado</th>
-                    <th>Puntaje</th>
-                    <th>Más info</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(i, index) in Juego" :key="index">
-                    <td>{{ i.nombre }}</td>
-                    <td>{{ i.plataforma }}</td>
-                    <td>{{ i.estado }}</td>
-                    <td>{{ i.puntaje }}</td>
-                    <td><button @click="seleccionarJuego(juegos)">Más Info</button></td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="tabla-videojuego">
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Plataforma</th>
+            <th>Estado</th>
+            <th>Puntaje</th>
+            <th>Acción</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(videojuego, index) in videojuegos" :key="index">
+            <td>{{ videojuego.nombre }}</td>
+            <td>{{ videojuego.plataforma }}</td>
+            <td>{{ videojuego.estado }}</td>
+            <td>{{ videojuego.puntaje }}</td>
+            <td><button @click="seleccionarVideojuego(videojuego)">Mostrar</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-</template>
-
-<script>
-import { juegosGuardados, juegosGuardados } from '../stores/DatosJuegos';
-
-export default {
+  </template>
+  
+  <script>
+  import { useVideojuegoStore } from '../stores/videojuegoStore'
+  
+  export default {
     props: {
-       juegoSeleccionado:{
+      onSeleccionarVideojuego: {
         type: Function,
         required: true
-       } 
+      }
     },
-
-    setup(props){
-        const juegosGuardados = juegosGuardados()
-
-        const seleccionarJuego = (juego) => {
-            props.onSeleccionarJuego(juego)
-        }
-        return {Juego: juegosGuardados.Juego, seleccionarJuego}
+    setup(props) {
+      const videojuegoStore = useVideojuegoStore()
+  
+      const seleccionarVideojuego = (videojuego) => {
+        props.onSeleccionarVideojuego(videojuego)
+      }
+  
+      return { videojuegos: videojuegoStore.videojuegos, seleccionarVideojuego }
     }
-}
-</script>
+  }
+  </script>
 
 <style>
 button {
